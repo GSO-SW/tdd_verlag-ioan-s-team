0061028d0df011199a85350f4d8a6a617f4e7856
sencer <hafsasencer@gmail.com>
2023-01-13 12:27:13 +0100

    Buch Classe added. get blocks added.

---
+++ Verlag/Buch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Verlag
+{
+    internal class Buch
+    {
+        private string Autor;
+        private string Titel;
+        private int Auflage;
+
+        public Buch(string autor, string titel, int auflage)
+        {
+            this.Autor = autor;
+            this.Titel = titel;
+            this.Auflage = auflage;
+        }
+
+        public string autor   // property
+        {
+            get { return autor; }   // get method
+        }
+
+        public string titel   // property
+        {
+            get { return titel; }   // get method
+        }
+
+        public int auflage   // property
+        {
+            get { return auflage; }   // get method
+        }
+
+
+    }
+}
