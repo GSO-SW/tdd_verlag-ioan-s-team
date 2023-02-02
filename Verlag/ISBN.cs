2a6f89ab455da070e6b838bca6b640fcd8c1bf8c
Ioan-Iancu <ionut.iancu01@gmail.com>
2023-02-02 21:40:56 +0100

    Das Code der für die Klasse ISBN gedacht war ist jetzt in dem ISBN Klasse. Außerdem allt Tests sind Grün.

--- Verlag/ISBN.cs
+++ Verlag/ISBN.cs
@@ -1,11 +1,70 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+
 namespace Verlag
 {
 	public class ISBN
 	{
+        public string isbnIntern;
+
 		public ISBN()
 		{
 		}
-	}
+
+
+        public string isbn
+        {
+            get
+            {
+                return isbnIntern;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    int rechnungsErgebniss = 0;
+                    for (int i = 0; i < 13; i++)
+                    {
+                        if (i == 0 || i == 2 || i == 5 || i == 7 || i == 9 || i == 11)
+                        {
+                            rechnungsErgebniss += Convert.ToInt32(value[i].ToString());
+                        }
+                        else if (i == 1 || i > 3)
+                        {
+                            rechnungsErgebniss += Convert.ToInt32(value[i].ToString()) * 3;
+                        }
+                    }
+                    int pruefziffer = 10 - (rechnungsErgebniss % 10);
+                    isbnIntern = value + pruefziffer;
+                }
+                else
+                {
+                    isbnIntern = value;
+                }
+            }
+        }
+
+        public string ISBN10()
+        {
+            string isbn10OhnePruefZiffer = "";
+            string isbn10 = "";
+            int isbn10PruefzifferOhneModulo = 0;
+            int isbn10Pruefziffer = 0;
+            for (int i = 4; i < 13; i++)
+            {
+                isbn10OhnePruefZiffer += isbnIntern[i];
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                isbn10PruefzifferOhneModulo += Convert.ToInt32(isbn10OhnePruefZiffer[i].ToString()) * (i + 1);
+            }
+
+            isbn10Pruefziffer = isbn10PruefzifferOhneModulo % 11;
+
+            isbn10 = isbn10OhnePruefZiffer + isbn10Pruefziffer.ToString();
+            return isbn10;
+        }
+    }
 }
 
--- VerlagTests/ISBNTests.cs
+++ VerlagTests/ISBNTests.cs
@@ -11,31 +11,31 @@ namespace VerlagTests
 		public void ISBN_KannEingegebenWerden()
 		{
 			//Arrange
-			Buch b = new Buch("Ioan Iancu", "titel");
+			ISBN b = new ISBN();
 			//Act
-			b.ISBN = "978-3770436163";
+			b.isbn = "978-3770436163";
 			//Assert
-			Assert.AreEqual("978-3770436163", b.ISBN);
+			Assert.AreEqual("978-3770436163", b.isbn);
         }
 
 		[TestMethod]
 		public void ISBN_PrueffzifferErgenzen()
 		{
 			//Arrange
-			Buch b = new Buch("Ioan Iancu", "titel");
+			ISBN b = new ISBN();
 			//Act
-			b.ISBN = "978-377043616";
+			b.isbn = "978-377043616";
 			//b.ISBN = "978-381582086";
 			//Assert
-			Assert.AreEqual("978-3770436163", b.ISBN);
+			Assert.AreEqual("978-3770436163", b.isbn);
         }
 
 		[TestMethod]
 		public void ISBN_FromISBN13ToISBN10()
 		{
 			//Arrange
-			Buch b = new Buch("Ioan Iancu", "title");
-			b.ISBN = "978-3770436064";
+			ISBN b = new ISBN();
+			b.isbn = "978-3770436064";
 			//Act
 			string ISBN10 = b.ISBN10();
 			//Assert
