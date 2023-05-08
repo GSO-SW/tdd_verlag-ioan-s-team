a89aec8bda62b143bd0ec460f2f95b9cc16208bd
Ioan Iancu <ionut.iancu01@gmail.com>
2023-05-08 18:58:30 +0200

    Buch_KannISBNBekommen ist erstelln worden als Test

--- VerlagTests/ISBNTests.cs
+++ VerlagTests/ISBNTests.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verlag;
 
 namespace VerlagTests
 {
-    internal class ISBNTests
+    [TestClass]
+    public class ISBNTests
     {
+        [TestMethod]
+        public void Buch_KannISBNBekommen()
+        {
+            //Arrange
+            string isbn = "978-3770436163";
+
+            //Act
+            Buch b = new Buch("autor", "titel", isbn);
+            Buch c = new Buch("autor", "titel", 14, isbn);
+
+            //Assert
+            Assert.AreEqual(isbn, b.ISBN);
+            Assert.AreEqual(isbn, c.ISBN);
+        }
     }
 }
