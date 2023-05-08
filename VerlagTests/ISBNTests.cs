737dfcccab1cbb7c930cb974350b28824172aa6d
Ioan Iancu <ionut.iancu01@gmail.com>
2023-05-08 19:06:48 +0200

    Tests Pruefziffer_KannErrechnetWerden und ISBN13zuISBN10 wurden erstelt

--- VerlagTests/ISBNTests.cs
+++ VerlagTests/ISBNTests.cs
@@ -25,5 +25,39 @@ namespace VerlagTests
             Assert.AreEqual(isbn, b.ISBN);
             Assert.AreEqual(isbn, c.ISBN);
         }
+
+        [TestMethod]
+        public void Pruefziffer_KannErrechnetWerden()
+        {
+            //Arrange 
+            string isbnOhnePrufeziffer = "978-377043614";
+            string isbnMitPruefziffer = "978-3770436149";
+
+            //Act 
+            Buch b = new Buch("Titel", "Autor", isbnOhnePrufeziffer);
+            Buch c = new Buch("Autor", "Titel", 25, isbnOhnePrufeziffer);
+
+            //Asseert
+            Assert.AreEqual(isbnMitPruefziffer, b.ISBN);
+            Assert.AreEqual(isbnMitPruefziffer, c.ISBN);
+
+        }
+
+        [TestMethod]
+        public void ISBN13zuISBN10()
+        {
+            //Arrange 
+            string isbn13 = "978-3770436064";
+            Buch b = new Buch("Titel", "Autor", isbn13);
+            string ergebnis = "3770436067";
+
+            //Act
+            string isbn10 = b.ISBN10Umrechnen();
+
+            //Assert
+            Assert.AreEqual(ergebnis, isbn10);
+        }
+
+
     }
 }
